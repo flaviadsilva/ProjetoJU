6c03e2059fe37b6484fd3a4ee9cc088e9fd05b2a
flaviadsilva <flavvs2@gmail.com>
2026-05-06 20:33:44 -0300

    config commit

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$dbname = 'PROJETOJU';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erro na conexao: " . $e->getMessage());
+}
